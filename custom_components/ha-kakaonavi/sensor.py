3534fb75245b733cc13c2a6ee4e2ffe02eb5e9d8
클라시커 <catalan39@naver.com>
2024-07-18 18:54:29 +0900

    NoneType 오류를 대응하고, 센서 이름 작명 단순화

--- custom_components/ha-kakaonavi/sensor.py
+++ custom_components/ha-kakaonavi/sensor.py
@@ -1,39 +1,56 @@
-from homeassistant.components.sensor import SensorEntity
+from homeassistant.components.sensor import SensorEntity, SensorDeviceClass
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from homeassistant.const import UnitOfTime
 from .const import DOMAIN
 
 class KakaoNaviEtaSensor(CoordinatorEntity, SensorEntity):
-    def __init__(self, coordinator, config_entry):
+    _attr_has_entity_name = True
+    _attr_name = None
+
+    def __init__(self, coordinator, config_entry, index):
         super().__init__(coordinator)
         self._config_entry = config_entry
-        self._attr_unique_id = f"{config_entry.entry_id}"
-        self._attr_name = f"Kakao Navi ETA: {config_entry.data['start']} to {config_entry.data['end']}"
+        self._attr_unique_id = f"{config_entry.entry_id}_{index}"
+        self._attr_device_info = {
+            "identifiers": {(DOMAIN, config_entry.entry_id)},
+            "name": f"Kakao Navi Route {index}",
+            "manufacturer": "Kakao",
+        }
+        self._index = index - 1  # 인덱스를 0부터 시작하도록 조정
+
+    @property
+    def device_class(self):
+        return SensorDeviceClass.DURATION
+
+    @property
+    def native_unit_of_measurement(self):
+        return UnitOfTime.MINUTES
 
     @property
     def state(self):
-        if self.coordinator.data:
-            return round(self.coordinator.data["current"]["routes"][0]["summary"]["duration"] / 60, 2)
+        if self.coordinator.data and "current" in self.coordinator.data:
+            try:
+                return round(self.coordinator.data["current"]["routes"][self._index]["summary"]["duration"] / 60, 2)
+            except (KeyError, IndexError, TypeError):
+                return None
         return None
 
     @property
     def extra_state_attributes(self):
         if self.coordinator.data:
-            current_data = self.coordinator.data["current"]["routes"][0]["summary"]
-            future_data = self.coordinator.data["future"]["routes"][0]["summary"]
-            return {
-                "current_eta": round(current_data["duration"] / 60, 2),
-                "future_eta": round(future_data["duration"] / 60, 2),
-                "eta_difference": round((future_data["duration"] - current_data["duration"]) / 60, 2),
-                "distance": current_data["distance"],
-                "taxi_fare": current_data["fare"]["taxi"],
-                "toll_fare": current_data["fare"]["toll"],
-            }
+            try:
+                current_data = self.coordinator.data["current"]["routes"][self._index]["summary"]
+                future_data = self.coordinator.data["future"]["routes"][self._index]["summary"]
+                return {
+                    "current_eta": round(current_data["duration"] / 60, 2),
+                    "future_eta": round(future_data["duration"] / 60, 2),
+                    "eta_difference": round((future_data["duration"] - current_data["duration"]) / 60, 2),
+                    "distance": current_data["distance"],
+                    "taxi_fare": current_data["fare"]["taxi"],
+                    "toll_fare": current_data["fare"]["toll"],
+                }
+            except (KeyError, IndexError, TypeError):
+                return {}
         return {}
 
-    @property
-    def unit_of_measurement(self):
-        return "min"
-
-async def async_setup_entry(hass, config_entry, async_add_entities):
-    coordinator = hass.data[DOMAIN][config_entry.entry_id]
-    async_add_entities([KakaoNaviEtaSensor(coordinator, config_entry)])
+# async_setup_entry 함수는 __init__.py에서 처리하므로 여기서는 제거합니다.
