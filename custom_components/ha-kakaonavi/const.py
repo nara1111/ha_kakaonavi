6e65199d094eba827ba43b964a7544f511d58a46
HAN, Sang-uk <40167504+nara1111@users.noreply.github.com>
2024-07-19 07:56:46 +0900

    Update const.py

--- custom_components/ha-kakaonavi/const.py
+++ custom_components/ha-kakaonavi/const.py
@@ -1,10 +1,12 @@
 DOMAIN = "ha_kakaonavi"
 CONF_APIKEY = "apikey"
+CONF_ROUTES = "routes"
 CONF_START = "start"
 CONF_END = "end"
 CONF_WAYPOINT = "waypoint"
 CONF_UPDATE_INTERVAL = "update_interval"
 CONF_FUTURE_UPDATE_INTERVAL = "future_update_interval"
+CONF_ROUTE_NAME = "name"
 DEFAULT_UPDATE_INTERVAL = 10
 DEFAULT_FUTURE_UPDATE_INTERVAL = 60
 MAX_DAILY_CALLS = 5000
