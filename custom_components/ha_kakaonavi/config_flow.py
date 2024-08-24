9c9827caf9a9bd8c5f38fd931b5e447f54da5eb0
HAN, Sang-uk <catalunya1939@icloud.com>
2024-08-24 11:50:13 +0900

    hotfix

--- custom_components/ha_kakaonavi/config_flow.py
+++ custom_components/ha_kakaonavi/config_flow.py
@@ -1,10 +1,11 @@
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from .const import (
-    DOMAIN, CONF_APIKEY, CONF_ROUTE_NAME, CONF_START, CONF_END,
-    CONF_WAYPOINT, CONF_PRIORITY, PRIORITY_OPTIONS, PRIORITY_RECOMMEND
+    DOMAIN, CONF_APIKEY, CONF_ROUTE_NAME, CONF_START, CONF_END, CONF_WAYPOINT,
+    CONF_PRIORITY, PRIORITY_OPTIONS, PRIORITY_RECOMMEND,
+    CONF_UPDATE_INTERVAL, CONF_FUTURE_UPDATE_INTERVAL,
+    DEFAULT_UPDATE_INTERVAL, DEFAULT_FUTURE_UPDATE_INTERVAL
 )
 from .api import KakaoNaviApiClient
 
@@ -15,8 +16,7 @@ class KakaoNaviConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         errors = {}
         if user_input is not None:
             try:
-                session = async_get_clientsession(self.hass)
-                client = KakaoNaviApiClient(user_input[CONF_APIKEY], session)
+                client = KakaoNaviApiClient(user_input[CONF_APIKEY])
                 await self.hass.async_add_executor_job(client.test_api_key)
                 await self.async_set_unique_id(user_input[CONF_APIKEY])
                 self._abort_if_unique_id_configured()
@@ -32,9 +32,13 @@ class KakaoNaviConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 return self.async_create_entry(
                     title=self.hass.config.location_name,
                     data={CONF_APIKEY: user_input[CONF_APIKEY]},
-                    options={"routes": [route]}
+                    options={
+                        CONF_UPDATE_INTERVAL: DEFAULT_UPDATE_INTERVAL,
+                        CONF_FUTURE_UPDATE_INTERVAL: DEFAULT_FUTURE_UPDATE_INTERVAL,
+                        "routes": [route]
+                    }
                 )
-            except Exception:
+            except Exception as e:
                 errors["base"] = "invalid_api_key"
 
         return self.async_show_form(
@@ -70,12 +74,8 @@ class KakaoNaviOptionsFlow(config_entries.OptionsFlow):
         return self.async_show_menu(
             step_id="init",
             menu_options={
-                "edit_route": self.hass.translator.translate(
-                    "options.step.init.menu_options.edit_route"
-                ),
-                "add_route": self.hass.translator.translate(
-                    "options.step.init.menu_options.add_route"
-                ),
+                "edit_route": "Edit existing route",
+                "add_route": "Add new route"
             }
         )
 
@@ -84,15 +84,8 @@ class KakaoNaviOptionsFlow(config_entries.OptionsFlow):
         routes = self.config_entry.options.get("routes", [])
 
         if user_input is not None:
-            route_index = next(i for i, route in enumerate(routes) if route[CONF_ROUTE_NAME] == user_input["route_to_edit"])
-            edited_route = {
-                CONF_ROUTE_NAME: user_input[CONF_ROUTE_NAME],
-                CONF_START: user_input[CONF_START],
-                CONF_END: user_input[CONF_END],
-                CONF_WAYPOINT: user_input.get(CONF_WAYPOINT),
-                CONF_PRIORITY: user_input.get(CONF_PRIORITY, PRIORITY_RECOMMEND)
-            }
-            routes[route_index] = edited_route
+            route_index = user_input.pop("route_index")
+            routes[route_index] = user_input
             new_options = dict(self.config_entry.options)
             new_options["routes"] = routes
             return self.async_create_entry(title="", data=new_options)
@@ -101,7 +94,7 @@ class KakaoNaviOptionsFlow(config_entries.OptionsFlow):
         return self.async_show_form(
             step_id="edit_route",
             data_schema=vol.Schema({
-                vol.Required("route_to_edit"): vol.In(route_names),
+                vol.Required("route_index"): vol.In(range(len(routes))),
                 vol.Required(CONF_ROUTE_NAME): str,
                 vol.Required(CONF_START): str,
                 vol.Required(CONF_END): str,
