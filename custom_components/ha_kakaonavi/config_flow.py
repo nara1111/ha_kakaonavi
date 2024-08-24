d372c31bc87b377cf9e117039c3d64a4153db12a
HAN, Sang-uk <catalunya1939@icloud.com>
2024-08-24 11:40:32 +0900

    hotfix

--- custom_components/ha_kakaonavi/config_flow.py
+++ custom_components/ha_kakaonavi/config_flow.py
@@ -1,11 +1,10 @@
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from .const import (
-    DOMAIN, CONF_APIKEY, CONF_ROUTE_NAME, CONF_START, CONF_END, CONF_WAYPOINT,
-    CONF_PRIORITY, PRIORITY_OPTIONS, PRIORITY_RECOMMEND,
-    CONF_UPDATE_INTERVAL, CONF_FUTURE_UPDATE_INTERVAL,
-    DEFAULT_UPDATE_INTERVAL, DEFAULT_FUTURE_UPDATE_INTERVAL
+    DOMAIN, CONF_APIKEY, CONF_ROUTE_NAME, CONF_START, CONF_END,
+    CONF_WAYPOINT, CONF_PRIORITY, PRIORITY_OPTIONS, PRIORITY_RECOMMEND
 )
 from .api import KakaoNaviApiClient
 
@@ -16,7 +15,8 @@ class KakaoNaviConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         errors = {}
         if user_input is not None:
             try:
-                client = KakaoNaviApiClient(user_input[CONF_APIKEY])
+                session = async_get_clientsession(self.hass)
+                client = KakaoNaviApiClient(user_input[CONF_APIKEY], session)
                 await self.hass.async_add_executor_job(client.test_api_key)
                 await self.async_set_unique_id(user_input[CONF_APIKEY])
                 self._abort_if_unique_id_configured()
@@ -32,13 +32,9 @@ class KakaoNaviConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 return self.async_create_entry(
                     title=self.hass.config.location_name,
                     data={CONF_APIKEY: user_input[CONF_APIKEY]},
-                    options={
-                        CONF_UPDATE_INTERVAL: DEFAULT_UPDATE_INTERVAL,
-                        CONF_FUTURE_UPDATE_INTERVAL: DEFAULT_FUTURE_UPDATE_INTERVAL,
-                        "routes": [route]
-                    }
+                    options={"routes": [route]}
                 )
-            except Exception as e:
+            except Exception:
                 errors["base"] = "invalid_api_key"
 
         return self.async_show_form(
@@ -74,8 +70,12 @@ class KakaoNaviOptionsFlow(config_entries.OptionsFlow):
         return self.async_show_menu(
             step_id="init",
             menu_options={
-                "edit_route": "options.step.init.menu_options.edit_route",
-                "add_route": "options.step.init.menu_options.add_route"
+                "edit_route": self.hass.translator.translate(
+                    "options.step.init.menu_options.edit_route"
+                ),
+                "add_route": self.hass.translator.translate(
+                    "options.step.init.menu_options.add_route"
+                ),
             }
         )
 
@@ -84,8 +84,15 @@ class KakaoNaviOptionsFlow(config_entries.OptionsFlow):
         routes = self.config_entry.options.get("routes", [])
 
         if user_input is not None:
-            route_index = user_input.pop("route_index")
-            routes[route_index] = user_input
+            route_index = next(i for i, route in enumerate(routes) if route[CONF_ROUTE_NAME] == user_input["route_to_edit"])
+            edited_route = {
+                CONF_ROUTE_NAME: user_input[CONF_ROUTE_NAME],
+                CONF_START: user_input[CONF_START],
+                CONF_END: user_input[CONF_END],
+                CONF_WAYPOINT: user_input.get(CONF_WAYPOINT),
+                CONF_PRIORITY: user_input.get(CONF_PRIORITY, PRIORITY_RECOMMEND)
+            }
+            routes[route_index] = edited_route
             new_options = dict(self.config_entry.options)
             new_options["routes"] = routes
             return self.async_create_entry(title="", data=new_options)
@@ -94,7 +101,7 @@ class KakaoNaviOptionsFlow(config_entries.OptionsFlow):
         return self.async_show_form(
             step_id="edit_route",
             data_schema=vol.Schema({
-                vol.Required("route_index"): vol.In(range(len(routes))),
+                vol.Required("route_to_edit"): vol.In(route_names),
                 vol.Required(CONF_ROUTE_NAME): str,
                 vol.Required(CONF_START): str,
                 vol.Required(CONF_END): str,
@@ -102,8 +109,7 @@ class KakaoNaviOptionsFlow(config_entries.OptionsFlow):
                 vol.Optional(CONF_PRIORITY, default=PRIORITY_RECOMMEND): vol.In(PRIORITY_OPTIONS),
             }),
             errors=errors,
-            description_placeholders={"route_names": ", ".join(route_names)},
-            last_step=True
+            description_placeholders={"route_names": ", ".join(route_names)}
         )
 
     async def async_step_add_route(self, user_input=None):
@@ -125,5 +131,4 @@ class KakaoNaviOptionsFlow(config_entries.OptionsFlow):
                 vol.Optional(CONF_PRIORITY, default=PRIORITY_RECOMMEND): vol.In(PRIORITY_OPTIONS),
             }),
             errors=errors,
-            last_step=True
         )
